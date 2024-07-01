dff992c4c65f6598f7284578b924c4a88e9a403f
JaeSunEo <wotjs020708@gmail.com>
2024-07-01 16:25:30 +0900

    initailize ConcurrencyApp

---
+++ ConcurrencyApp/ConcurrencyApp/ConcurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyAppApp.swift
+//  ConcurrencyApp
+//
+//  Created by 어재선 on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
