07824d538897a690fcaf773831e97a529c5a4540
JaeSunEo <wotjs020708@gmail.com>
2024-07-01 10:38:48 +0900

    initialize Project Debugging

---
+++ Debugging/Debugging/DebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebuggingApp.swift
+//  Debugging
+//
+//  Created by 어재선 on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct DebuggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
