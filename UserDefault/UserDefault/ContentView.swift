6349b25f19555250908b180bf9d57ef9e2e13dce
JaeSunEo <wotjs020708@gmail.com>
2024-07-02 10:04:59 +0900

    UserDefaults

--- UserDefault/UserDefault/ContentView.swift
+++ UserDefault/UserDefault/ContentView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var myText = ""
+    @State private var myToggle = true
+    @State private var mySlider = 0.0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Change text here", text: $myText)
+            Toggle(isOn: $myToggle) {
+                Text("Toggle here")
+            }
+            Slider(value: $mySlider)
+            Button(action: {
+                UserDefaults.standard.set(myText, forKey: "Text")
+                UserDefaults.standard.set(myToggle, forKey: "Toggle")
+                UserDefaults.standard.set(mySlider, forKey: "Slider")
+            }) {
+                Text("Save data")
+            }
+            Button(action: {
+                myText = ""
+                myToggle = true
+                mySlider = 0.0
+            }) {
+                Text("Clear data")
+            }
+            Button(action: {
+                myText = UserDefaults.standard.string(forKey: "Text") ?? ""
+                myToggle = UserDefaults.standard.bool(forKey: "Toggle")
+                mySlider = UserDefaults.standard.double(forKey: "Slider")
+            }) {
+                Text("Retrive data")
+            }
         }
         .padding()
     }
