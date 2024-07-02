5d931a7ab24cd5187c5690ce5d886409664b0d14
JaeSunEo <wotjs020708@gmail.com>
2024-07-02 15:35:04 +0900

    BindingApp init

---
+++ BindingApp/BindingApp/BindingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingAppApp.swift
+//  BindingApp
+//
+//  Created by 어재선 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BindingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
