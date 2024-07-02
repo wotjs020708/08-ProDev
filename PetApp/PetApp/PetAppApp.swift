51e8d69ad0c148480fd60f92faecfd4fecb9135e
JaeSunEo <wotjs020708@gmail.com>
2024-07-02 13:34:48 +0900

    initialize Pet App

---
+++ PetApp/PetApp/PetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetAppApp.swift
+//  PetApp
+//
+//  Created by 어재선 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
