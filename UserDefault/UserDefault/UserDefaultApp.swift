7713ae6678748180d7d4a43966069ddda9d76618
JaeSunEo <wotjs020708@gmail.com>
2024-07-02 09:25:14 +0900

    initailize UserDefault

---
+++ UserDefault/UserDefault/UserDefaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultApp.swift
+//  UserDefault
+//
+//  Created by 어재선 on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
