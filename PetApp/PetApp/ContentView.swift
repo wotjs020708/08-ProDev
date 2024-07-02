f2a3e1e11ab7baf8b30101dd97bf831a6e03a214
JaeSunEo <wotjs020708@gmail.com>
2024-07-02 14:23:43 +0900

    List 출력

--- PetApp/PetApp/ContentView.swift
+++ PetApp/PetApp/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     @State var petName = ""
     @State var petBreed = ""
-    @State var patArray = [Animal]()
+    @State var petArray = [Animal]()
     var body: some View {
         VStack {
             TextField("Enter pet name", text: $petName)
@@ -20,12 +20,27 @@ struct ContentView: View {
             Button("Save") {
                 coreDM.savePet(name: petName, breed: petBreed)
                 
+                displayPets()
                 petName = ""
                 petBreed = ""
             }
-            
+            List {
+                ForEach(petArray, id:\.self) { pet in
+                    VStack {
+                        Text(pet.name ?? "")
+                        Text(pet.breed ?? "")
+                    }
+                }
+            }
         }
         .padding()
+        .onAppear() {
+            displayPets()
+        }
+    }
+    
+    func displayPets() {
+        petArray = coreDM.getAllPets()
     }
 }
 
