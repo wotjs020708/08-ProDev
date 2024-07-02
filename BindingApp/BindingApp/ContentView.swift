f30ee462d06b328960a41ac44aced40df6cc8d8b
JaeSunEo <wotjs020708@gmail.com>
2024-07-02 15:36:57 +0900

    초기 UI 추가

--- BindingApp/BindingApp/ContentView.swift
+++ BindingApp/BindingApp/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, \(message)")
+            HStack {
+                Text("Send a greeting:")
+                TextField("Type a message here", text: $message)
+            }
         }
         .padding()
     }
