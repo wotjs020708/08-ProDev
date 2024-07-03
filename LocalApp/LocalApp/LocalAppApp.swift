4131ecb7d5ec71ec0bc7dca037652a1da99ca169
JaeSunEo <wotjs020708@gmail.com>
2024-07-03 09:27:10 +0900

    initialize Project LocalApp

---
+++ LocalApp/LocalApp/LocalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalAppApp.swift
+//  LocalApp
+//
+//  Created by 어재선 on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
