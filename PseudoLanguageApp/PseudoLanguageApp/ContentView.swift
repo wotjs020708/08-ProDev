ad5809556db35f44ff70bcb47e2afd935f8f476c
JaeSunEo <wotjs020708@gmail.com>
2024-07-03 11:08:19 +0900

    Text 추갸

--- PseudoLanguageApp/PseudoLanguageApp/ContentView.swift
+++ PseudoLanguageApp/PseudoLanguageApp/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world")
+                .padding()
+                .background(Color.yellow)
+            
+            Text("This is a lot of text to display in a small amount of sace to test how well Xcode can truncate large amounts of text.")
+                .padding()
+                .background(Color.mint)
         }
         .padding()
     }
