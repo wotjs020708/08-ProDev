49f9268b2daf2483b74c7e2c76e61db891052d8a
JaeSunEo <wotjs020708@gmail.com>
2024-07-03 13:28:13 +0900

    SearchScope

--- SearchApp/SearchApp/ContentView.swift
+++ SearchApp/SearchApp/ContentView.swift
@@ -7,7 +7,14 @@
 
 import SwiftUI
 
+enum ProductScope {
+    case fruit
+    case vegetable
+}
+
 struct ContentView: View {
+    @State private var scope: ProductScope = .fruit
+    
     @State var searchText = ""
     
     let petArray = ["Cat", "Dog", "Fish", "Donkey", "Canary", "Camel", "Frog"]
@@ -21,17 +28,21 @@ struct ContentView: View {
                     placement: .navigationBarDrawer(displayMode: .always),
                     prompt: "Look for a pet"
         ) {
-            Text("singing").searchCompletion("Canary")
-            Text("Croaking").searchCompletion("Forg")
-            Text("Grumpy").searchCompletion("Cat")
-            
-            Divider()
+//            Text("singing").searchCompletion("Canary")
+//            Text("Croaking").searchCompletion("Forg")
+//            Text("Grumpy").searchCompletion("Cat")
+//            
+//            Divider()
             // serchText 가 비었을 경우, hasPrefix는 true 를 리턴
             // 배열의 모든 요소가 출력됨
             ForEach(petArray.filter { !searchText.isEmpty && $0.hasPrefix(searchText) }, id: \.self) {
                 name in
                 Text(name)
             }
+        }
+        .searchScopes($scope) {
+            Text("Fruit").tag(ProductScope.fruit)
+            Text("Vegetable").tag(ProductScope.vegetable)
         }
         
     }
@@ -46,8 +57,7 @@ struct PetListView: View {
     var body: some View {
         List(animals, id: \.self) { animals in
             Text(animals)
-                .background(.yellow)
         }
-//        .listStyle(.plain)
+        .listStyle(.plain)
     }
 }
