d6fb6b179182896334aac78fd2057a40d005a219
JaeSunEo <wotjs020708@gmail.com>
2024-07-03 11:05:05 +0900

    Initailze Project PsudoLanguage

---
+++ PseudoLanguageApp/PseudoLanguageApp/PseudoLanguageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PseudoLanguageAppApp.swift
+//  PseudoLanguageApp
+//
+//  Created by 어재선 on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PseudoLanguageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
