a1cddd9f3054c2033e6d7b491f86f6e1bf3f184d
JaeSunEo <wotjs020708@gmail.com>
2024-07-03 13:37:23 +0900

    Tag 검색 예제

--- SearchApp/SearchApp/ContentView.swift
+++ SearchApp/SearchApp/ContentView.swift
@@ -7,37 +7,43 @@
 
 import SwiftUI
 
+
+struct Pet: Identifiable {
+    let id = UUID()
+    let name: String
+    let tags: [String]
+}
+
+
 struct ContentView: View {
     @State private var searchText = ""
-    @State private var searchScope = 0
     
-    let fruits = ["Apple", "Banana", "Cherry", "Date", "Fig", "Orange"]
-    let scopes = ["All", "A-M", "N-Z"]
-    
-    var filteredFruits: [String] {
-        let filtered = fruits.filter { searchText.isEmpty || $0.localizedCaseInsensitiveContains(searchText) }
-        switch searchScope {
-        case 1: return filtered.filter { $0.prefix(1).localizedCaseInsensitiveCompare("N") == .orderedAscending }
-        case 2: return filtered.filter { $0.prefix(1).localizedCaseInsensitiveCompare("N") != .orderedAscending }
-        default: return filtered
+    let pets = [
+        Pet(name: "Cat", tags: ["Land"]),
+        Pet(name: "Dog", tags: ["Land"]),
+        Pet(name: "Fish", tags: ["Water"]),
+        Pet(name: "Donkey", tags: ["Land"]),
+        Pet(name: "Canary", tags: ["Air"]),
+        Pet(name: "Camel", tags: ["Land"]),
+        Pet(name: "Frog", tags: ["Water", "Land"])
+    ]
+    var filteredPets: [Pet] {
+        if searchText.isEmpty {
+            return pets
+        } else {
+            return pets.filter { $0.name.localizedCaseInsensitiveContains(searchText) || $0.tags.contains(where: { $0.localizedCaseInsensitiveContains(searchText) }) }
         }
     }
     var body: some View {
-        NavigationStack{
-            List(filteredFruits, id: \.self) { fruit in
-                Text(fruit)
+        NavigationStack {
+            List(filteredPets) { pet in
+                Text(pet.name)
             }
-            .navigationTitle("Fruits")
-            .searchable(text: $searchText)
-            .searchScopes($searchScope) {
-                ForEach(0..<scopes.count, id: \.self) { index in
-                    Text(scopes[index]).tag(index)
-                }
-                
-            }
+            .navigationTitle("SearchApp")
+            .searchable(text: $searchText, prompt: "Look for a pet")
         }
     }
 }
-        #Preview {
-            ContentView()
-        }
+#Preview {
+    ContentView()
+}
