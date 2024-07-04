a2ec1c4228f8f6b14eda1120f4422a976b037858
JaeSunEo <wotjs020708@gmail.com>
2024-07-04 17:41:49 +0900

    공유 링크 생성

--- SimpleMapApp/SimpleMapApp/MapViewModel.swift
+++ SimpleMapApp/SimpleMapApp/MapViewModel.swift
@@ -56,7 +56,30 @@ class MapViewModel: NSObject, CLLocationManagerDelegate {
     }
     
     func shareLocation() {
-        print("share locaiton")
+        guard let selectedPlace = selectedPlace else { return }
+        let coordinate = selectedPlace.placemark.coordinate
+        let placeName = selectedPlace.name ?? "선택된 위치"
+        
+        // 지도 링크 생성
+        let mapLink = "http://maps.apple.com/?ll=\(coordinate.latitude),\(coordinate.longitude)"
+        
+        // 공유 텍스트 생성
+        let shareText = "\(placeName) = 지도 링크: \(mapLink)"
+        
+        let activityViewController = UIActivityViewController(activityItems: [shareText], applicationActivities: nil)
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let rootViewController = windowScene.windows.first?.rootViewController {
+            
+            if let popoverPresentationController = activityViewController.popoverPresentationController {
+                popoverPresentationController.sourceView = rootViewController.view
+                popoverPresentationController.sourceRect = CGRect(x: rootViewController.view.bounds.midX, y: rootViewController.view.bounds.midY, width: 0, height: 0)
+                popoverPresentationController.permittedArrowDirections = []
+            }
+            
+            rootViewController.present(activityViewController, animated: true, completion: nil)
+        }
+        
     }
     
     //MARK: - CLLocationManagerDelegate
