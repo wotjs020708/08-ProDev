7c568886688bb2bc4a1cd625b7cd3942964bfa71
JaeSunEo <wotjs020708@gmail.com>
2024-07-04 09:40:55 +0900

    Initialize MapApp

---
+++ MapApp/MapApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MapApp
+//
+//  Created by 어재선 on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
