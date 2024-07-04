cbbf26e0e7563ac16e7434dbbc23f1902b05ce43
JaeSunEo <wotjs020708@gmail.com>
2024-07-04 16:38:37 +0900

    CoreLocation 위치정보 업데이트

--- SimpleMapApp/SimpleMapApp/ContentView.swift
+++ SimpleMapApp/SimpleMapApp/ContentView.swift
@@ -19,29 +19,47 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack {
-            Map(position: $viewModel.cameraPosition)
-                .mapStyle(viewModel.mapStyle)
-                .searchable(text: $viewModel.searchText)
-                .searchScopes($style){
-                    Text("Standard").tag(0)
-                    Text("Imagery").tag(1)
-                    Text("Hybrid").tag(2)
-                }
-                .onChange(of: style) {
-                    switch style {
-                    case 0:
-                        viewModel.mapStyle = .standard
-                        
-                    case 1:
-                        viewModel.mapStyle = .imagery
-                        
-                    case 2:
-                        viewModel.mapStyle = .hybrid
-                        
-                    default:
-                        viewModel.mapStyle = .standard
+            ZStack(alignment: .bottom) {
+                Map(position: $viewModel.cameraPosition)
+                    .mapStyle(viewModel.mapStyle)
+                    .searchable(text: $viewModel.searchText)
+                    .searchScopes($style){
+                        Text("Standard").tag(0)
+                        Text("Imagery").tag(1)
+                        Text("Hybrid").tag(2)
                     }
+                    .mapControls{
+                        MapScaleView()
+                        MapUserLocationButton()
+                    }
+                    .onChange(of: style) {
+                        switch style {
+                        case 0:
+                            viewModel.mapStyle = .standard
+                            
+                        case 1:
+                            viewModel.mapStyle = .imagery
+                            
+                        case 2:
+                            viewModel.mapStyle = .hybrid
+                            
+                        default:
+                            viewModel.mapStyle = .standard
+                        }
                 }
+                HStack {
+                    Button(action: {
+                        viewModel.moveToCurrentLocation()
+                    }) {
+                        Image(systemName: "location.fill")
+                            .padding()
+                            .background(.white)
+                            .clipShape(Circle())
+                    }
+                    Spacer()
+                }
+                .padding()
+            }
             
             
         }
--- SimpleMapApp/SimpleMapApp/MapViewModel.swift
+++ SimpleMapApp/SimpleMapApp/MapViewModel.swift
@@ -9,10 +9,29 @@ import SwiftUI
 import MapKit
 
 @Observable
-class MapViewModel {
+class MapViewModel: NSObject, CLLocationManagerDelegate {
     var cameraPosition: MapCameraPosition = .userLocation(fallback: .automatic)
     var searchText = ""
     var mapStyle: MapStyle = .standard
     
+    private var locaitonManager: CLLocationManager = CLLocationManager()
     
+    override init() {
+        super.init()
+        locaitonManager.delegate = self
+    }
+    
+    func moveToCurrentLocation() {
+        cameraPosition = .userLocation(fallback: .automatic)
+    }
+    
+    //MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("error \(error.localizedDescription)")
+    }
 }
