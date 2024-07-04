3eba3ecaa3238c7b6d0c9b0245d0fc7f728ddbae
JaeSunEo <wotjs020708@gmail.com>
2024-07-04 10:27:00 +0900

    기존 Map Marker 추가

--- MapApp/MapApp/ContentView.swift
+++ MapApp/MapApp/ContentView.swift
@@ -12,17 +12,36 @@ extension CLLocationCoordinate2D {
     static let paris = CLLocationCoordinate2D(latitude: 48.856613, longitude: 2.352222)
 }
 
+struct IdentifiablePlace: Identifiable {
+    let id: UUID
+    let location: CLLocationCoordinate2D
+    init(id: UUID = UUID(), lat: Double, long: Double) {
+        self.id = id
+        self.location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        
+    }
+}
+
 
 struct ContentView: View {
     let locationManger = CLLocationManager()
     
     @State var message = "Map of Paris"
     @State private var position: MapCameraPosition = .region(MKCoordinateRegion(center: .paris, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)))
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: .paris, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
     
+    let place = IdentifiablePlace(lat: 48.856613, long: 2.352222)
     
     var body: some View {
         
         VStack{
+            // iOS 17 이전 코드
+            Map(coordinateRegion: $region,
+                annotationItems: [place]) { place in
+                MapMarker(coordinate: place.location,
+                          tint: Color.purple)
+            }
+            // iOS 17 이후 코드
             Map(position: $position){
                 Annotation("에펠탑", coordinate: .paris) {
                     Image(systemName: "mappin.circle.fill")
@@ -31,7 +50,7 @@ struct ContentView: View {
                         .clipShape(Circle())
                 }
             }
-                
+            
             TextEditor(text:  $message)
                 .frame(width: .infinity, height: 100)
         }
