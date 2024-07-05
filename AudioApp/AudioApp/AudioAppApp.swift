26b79af3e08621552eeb7617391bbfac75eab15e
JaeSunEo <wotjs020708@gmail.com>
2024-07-05 10:09:49 +0900

    Initialize Project AudioApp

---
+++ AudioApp/AudioApp/AudioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioAppApp.swift
+//  AudioApp
+//
+//  Created by 어재선 on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
