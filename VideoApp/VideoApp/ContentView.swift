db65c17ae6ed8e8260cbccd98dca6d8de64a35fe
JaeSunEo <wotjs020708@gmail.com>
2024-07-05 11:05:13 +0900

    비도오 오버레이

--- VideoApp/VideoApp/ContentView.swift
+++ VideoApp/VideoApp/ContentView.swift
@@ -11,7 +11,15 @@ import AVKit
 struct ContentView: View {
     @State var player: AVPlayer?
     var body: some View {
-            VideoPlayer(player: player)
+        
+        // VideoPlayer(player: player, videoOverlay: { })
+        // 아래 코드처럼 후행 클로저 사용 가능
+        VideoPlayer(player: player) {
+            VStack {
+                Text("Overlay text to appear")
+                    .foregroundStyle(.white)
+            }
+        }
             .frame(height: 320)
             .onAppear {
                 guard let videoURL = Bundle.main.url(forResource: "SaturnV", withExtension: "mov") else {
