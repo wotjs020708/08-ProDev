52b86ee5c21050dda02e94fec92acb3c32c0f422
JaeSunEo <wotjs020708@gmail.com>
2024-07-05 11:23:45 +0900

    Initialize SimpleMediaApp

---
+++ SimpleMediaApp/SimpleMediaApp/SimpleMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMediaAppApp.swift
+//  SimpleMediaApp
+//
+//  Created by 어재선 on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
