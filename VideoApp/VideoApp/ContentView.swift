601ccfb97d883540dc47a9cd27d9e42757adebb0
JaeSunEo <wotjs020708@gmail.com>
2024-07-05 10:47:35 +0900

    Video Player

--- VideoApp/VideoApp/ContentView.swift
+++ VideoApp/VideoApp/ContentView.swift
@@ -6,16 +6,20 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State var player: AVPlayer?
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            VideoPlayer(player: player)
+            .frame(height: 320)
+            .onAppear {
+                guard let videoURL = Bundle.main.url(forResource: "SaturnV", withExtension: "mov") else {
+                    print("Video file not found")
+                    return
+                }
+                player = AVPlayer(url: videoURL as URL)
+            }
     }
 }
 
