d108620362abdbf40b3981328dca3411f163253e
JaeSunEo <wotjs020708@gmail.com>
2024-07-08 11:36:04 +0900

    Initialize Project PDFApp

---
+++ PDFApp/PDFApp/PDFAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFAppApp.swift
+//  PDFApp
+//
+//  Created by 어재선 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PDFAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
