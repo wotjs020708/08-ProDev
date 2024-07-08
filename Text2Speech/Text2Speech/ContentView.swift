ca8c7199adcf672341dfe8588fb24fd9c0e189ce
JaeSunEo <wotjs020708@gmail.com>
2024-07-08 11:16:27 +0900

    텍스트 -> 음성

--- Text2Speech/Text2Speech/ContentView.swift
+++ Text2Speech/Text2Speech/ContentView.swift
@@ -6,14 +6,27 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    let audio = AVSpeechSynthesizer()
+    
+    @State var converText = AVSpeechUtterance(string: "")
+    @State var textToRoad = "This is a test of the emergency broadcast system"
+    @State var sliderValue: Float = 0.0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextEditor(text: $textToRoad)
+                .frame(width: 250,height: 200)
+            Slider(value: $sliderValue, in: 0...10)
+            Button {
+                converText = AVSpeechUtterance(string: textToRoad)
+                converText.rate = sliderValue
+                audio.speak(converText)
+            } label: {
+                Text("Read Text Out Loud")
+            }
         }
         .padding()
     }
