12fe77f93f4ec5a8669a98e1560c830b0c99fa2f
JaeSunEo <wotjs020708@gmail.com>
2024-07-08 11:27:05 +0900

    다양한 목소리 테스트

--- Text2Speech/Text2Speech/ContentView.swift
+++ Text2Speech/Text2Speech/ContentView.swift
@@ -11,19 +11,29 @@ import AVFoundation
 struct ContentView: View {
     let audio = AVSpeechSynthesizer()
     
-    @State var converText = AVSpeechUtterance(string: "")
+    @State var convertText = AVSpeechUtterance(string: "")
     @State var textToRoad = "This is a test of the emergency broadcast system"
-    @State var sliderValue: Float = 0.0
-
+    @State var sliderValue: Float = 0.5
+    
     var body: some View {
         VStack {
             TextEditor(text: $textToRoad)
                 .frame(width: 250,height: 200)
             Slider(value: $sliderValue, in: 0...10)
             Button {
-                converText = AVSpeechUtterance(string: textToRoad)
-                converText.rate = sliderValue
-                audio.speak(converText)
+                convertText = AVSpeechUtterance(string: textToRoad)
+                convertText.rate = sliderValue
+                
+                // Retrieve the British English voice.
+                let voice = AVSpeechSynthesisVoice(language: "en-GB")
+                // 다양한 음성 지원 (안되는 것도 있음)
+                // https://gist.github.com/Koze/d1de49c24fc28375a9e314c72f7fdae4
+                //                let voice = AVSpeechSynthesisVoice(identifier: "com.apple.ttsbundle.siri_male_en-GB_compact")
+                // Assign the voice to the utterance.
+                convertText.voice = voice
+                
+                
+                audio.speak(convertText)
             } label: {
                 Text("Read Text Out Loud")
             }
