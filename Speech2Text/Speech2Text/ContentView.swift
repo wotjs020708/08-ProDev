49961319b609c8d18e9f9eef8f84ff56370a6f78
JaeSunEo <wotjs020708@gmail.com>
2024-07-08 10:21:16 +0900

    음성 -> 텍스트 인식 기능

--- Speech2Text/Speech2Text/ContentView.swift
+++ Speech2Text/Speech2Text/ContentView.swift
@@ -6,16 +6,73 @@
 //
 
 import SwiftUI
+import Speech
 
 struct ContentView: View {
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ko-KR"))
+    
+    @State var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    @State var recognitionTask: SFSpeechRecognitionTask?
+    @State var message = ""
+    @State var buttonStatus = true
+    
+    func startRecording() {
+        message = "Start recording"
+        let node = audioEngine.inputNode
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        recognitionRequest?.shouldReportPartialResults = true
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) {
+            (buffer, _) in
+            recognitionRequest?.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            return print(error)
+        }
+        guard let recognizeMe = SFSpeechRecognizer() else {
+            return
+        }
+        if !recognizeMe.isAvailable {
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest ?? SFSpeechAudioBufferRecognitionRequest.init(), resultHandler: { result, error in
+            if let result = result {
+                let transcribedString = result.bestTranscription.formattedString
+                message = transcribedString
+            } else if let error = error {
+                print(error)
+            }
+        })
+        
+        
+    }
+    
+    func stopRecording() {
+        audioEngine.stop()
+        recognitionTask?.cancel()
+        audioEngine.inputNode.removeTap(onBus: 0)
+        recognitionRequest?.endAudio()
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           TextEditor(text: $message)
+                .frame(width: 350, height: 400)
+            Button(buttonStatus ? "Start recording" : "stop recrding", action: {
+                buttonStatus.toggle()
+                if buttonStatus {
+                    stopRecording()
+                } else {
+                    startRecording()
+                }
+            })
         }
         .padding()
+        .background()
     }
 }
 
