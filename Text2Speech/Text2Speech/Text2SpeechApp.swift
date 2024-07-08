b8b7c4ecdf041c0b7b2dab1a6641050f42432e57
JaeSunEo <wotjs020708@gmail.com>
2024-07-08 11:02:55 +0900

    Initialize Project Text2Speech

---
+++ Text2Speech/Text2Speech/Text2SpeechApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text2SpeechApp.swift
+//  Text2Speech
+//
+//  Created by 어재선 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Text2SpeechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
