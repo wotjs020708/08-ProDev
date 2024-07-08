963c4082102b852e395e859df38a50fcbc582c82
JaeSunEo <wotjs020708@gmail.com>
2024-07-08 15:24:05 +0900

    Initialize Project PhotosApp

---
+++ PhotosApp/PhotosApp/PhotosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosAppApp.swift
+//  PhotosApp
+//
+//  Created by 어재선 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- PictureApp/PictureApp/ContentView.swift
+++ PictureApp/PictureApp/ContentView.swift
@@ -8,15 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSheet = false
+    @State private var image = UIImage()
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(uiImage: self.image)
+                .resizable()
+                .scaledToFill()
+                .frame(minWidth: 0, maxWidth: .infinity)
+            
+            HStack{
+                Button {
+                    showSheet = true
+                    sourceType = .photoLibrary
+                } label: {
+                    Text("Photo library")
+                        .font(.title2)
+                }
+                
+                Spacer()
+                Button {
+                    showSheet = true
+                    sourceType = .camera
+                } label: {
+                    Text("Camera")
+                        .font(.title2)
+                }
+            }
         }
         .padding()
+        .sheet(isPresented: $showSheet) {
+            ImagePicker(sourceType: sourceType ,chosenImage: $image)
+        }
     }
+    
 }
 
 #Preview {
