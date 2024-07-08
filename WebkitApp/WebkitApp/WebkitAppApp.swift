da58d2b0eb2f32bec0049b0a665d262a87678282
JaeSunEo <wotjs020708@gmail.com>
2024-07-08 13:04:49 +0900

    WekKitApp

---
+++ WebkitApp/WebkitApp/WebkitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebkitAppApp.swift
+//  WebkitApp
+//
+//  Created by 어재선 on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WebkitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
