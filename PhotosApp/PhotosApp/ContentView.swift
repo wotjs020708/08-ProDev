76e9699420d02a88a17695cb225070a1819c2282
JaeSunEo <wotjs020708@gmail.com>
2024-07-08 15:44:44 +0900

    PhotosUI 사용 사진 라이브러리 추가

--- PhotosApp/PhotosApp/ContentView.swift
+++ PhotosApp/PhotosApp/ContentView.swift
@@ -6,16 +6,46 @@
 //
 
 import SwiftUI
+import PhotosUI
+
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var selectedImageData: Data?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let selectedImageData,
+               let uiImage = UIImage(data: selectedImageData) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+            } else {
+                Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+            }
+            
+            PhotosPicker(selection: $selectedItem, matching: .images, photoLibrary: .shared()) {
+                Text("Select a phto")
+                    .padding()
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .presentationCornerRadius(10)
+            }
+            
+            
         }
         .padding()
+        .onChange(of: selectedItem) {
+            Task {
+                if let data = try? await selectedItem?.loadTransferable(type: Data.self) {
+                    selectedImageData = data
+                }
+            }
+        }
     }
 }
 
