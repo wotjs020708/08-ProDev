22c93fd652e0cbed8b784dbe4dfce925266cc91f
JaeSunEo <wotjs020708@gmail.com>
2024-07-09 09:17:19 +0900

    Initialize Project facizlRecognitionApp

---
+++ FacialRecognitionApp/FacialRecognitionApp/FacialRecognitionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialRecognitionAppApp.swift
+//  FacialRecognitionApp
+//
+//  Created by 어재선 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FacialRecognitionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
