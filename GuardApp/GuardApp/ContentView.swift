f60575a1fbc27cde072ab8dccd2caa85a6b8d663
JaeSunEo <wotjs020708@gmail.com>
2024-07-09 15:07:52 +0900

    정상 작동 코드

--- GuardApp/GuardApp/ContentView.swift
+++ GuardApp/GuardApp/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showImage: UIImage = UIImage(systemName: "photo")!
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(uiImage: showImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 400)
+            Button {
+                getImage()
+                
+            } label: {
+                Text("Add picture")
+            }
         }
         .padding()
+    }
+    
+    func getImage() {
+        showImage = UIImage(named: "car")!
     }
 }
 
