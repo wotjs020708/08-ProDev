69725c396fa54c65b86d1978f4afd6a298ea74d2
JaeSunEo <wotjs020708@gmail.com>
2024-07-09 13:29:26 +0900

    Initialize Project CoreMLImageApp

---
+++ CoreMLImageApp/CoreMLImageApp/CoreMLImageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMLImageAppApp.swift
+//  CoreMLImageApp
+//
+//  Created by 어재선 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreMLImageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
