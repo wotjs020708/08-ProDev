2e70861fef935d0d7bc5ed1c7cc5fcd0dc930495
JaeSunEo <wotjs020708@gmail.com>
2024-07-09 15:04:31 +0900

    Initialize Project GuardApp

---
+++ GuardApp/GuardApp/GuardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuardAppApp.swift
+//  GuardApp
+//
+//  Created by 어재선 on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct GuardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
