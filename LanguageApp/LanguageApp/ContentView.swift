54dfbc9382e24a015cff0a1110ffb94f002f853d
JaeSunEo <wotjs020708@gmail.com>
2024-07-09 14:34:17 +0900

    언어 감지

--- LanguageApp/LanguageApp/ContentView.swift
+++ LanguageApp/LanguageApp/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import NaturalLanguage
 
 struct ContentView: View {
+
+    @State var language = ""
+    @State var messgae = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Type foreign text here", text: $language)
+                .autocorrectionDisabled()
+                .padding()
+            Text("Language = \(messgae)")
+                .padding()
+            Button {
+                if let prediction = predictLanguage(text: language) {
+                    messgae = prediction
+                }
+                    
+            } label: {
+                Text("Identify Language")
+            }
+            
         }
         .padding()
+    }
+    
+    func predictLanguage(text: String) -> String? {
+        let locale = Locale(identifier: "en")
+        let recognizer = NLLanguageRecognizer()
+        recognizer.processString(text)
+        guard let language = recognizer.dominantLanguage else {
+            return "Unknown language"
+        }
+        return locale.localizedString(forIdentifier: language.rawValue)
     }
 }
 
