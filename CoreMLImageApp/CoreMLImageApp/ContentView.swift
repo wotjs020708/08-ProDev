6dcf0b5b1144821ef7c068478ead1bddc5fd7d3c
JaeSunEo <wotjs020708@gmail.com>
2024-07-09 13:34:03 +0900

    UI작업

--- CoreMLImageApp/CoreMLImageApp/ContentView.swift
+++ CoreMLImageApp/CoreMLImageApp/ContentView.swift
@@ -6,17 +6,60 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
 struct ContentView: View {
+    
+    @State var message = ""
+    @State var arrayIndex = 0
+    @State var image: UIImage = UIImage(named: "cat")!
+    
+    let photoArray = ["cat", "plane", "banana", "car"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 250, height: 250)
+            TextEditor(text: $message)
+                .padding()
+            Button {
+                useAI(sentImage: photoArray[arrayIndex])
+            } label: {
+                Text("Analyze Image")
+            }.padding()
+            
+            HStack {
+                Button {
+                    if arrayIndex == 0 {
+                        arrayIndex = photoArray.count - 1
+                    } else {
+                        arrayIndex -= 1
+                    }
+                    message = ""
+                    image = UIImage(named: photoArray[arrayIndex])!
+                } label: {
+                    Image(systemName: "chevron.left.square.fill")
+                }
+                Button {
+                    if arrayIndex == photoArray.count - 1 {
+                        arrayIndex = 0
+                    } else {
+                        arrayIndex += 1
+                    }
+                    message = ""
+                    image = UIImage(named: photoArray[arrayIndex])!
+                } label: {
+                    Image(systemName: "chevron.right.square.fill")
+                }
+            }        }
         .padding()
     }
+    
+    func useAI(sentImage: String) {
+       }
 }
 
 #Preview {
